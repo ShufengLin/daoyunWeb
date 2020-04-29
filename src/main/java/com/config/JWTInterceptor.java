6f42b7e45f0ac36ffba2df121d30d9cf137d257c
czlPlay_glb <279747233@qq.com>
2020-04-29 12:22:07 +0800

    token解析，拦截器配置

--- src/main/java/com/config/JWTInterceptor.java
+++ src/main/java/com/config/JWTInterceptor.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.utils.ServerResponse;
+import com.utils.token.Decrypt;
 import com.utils.token.JedisUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -14,7 +16,7 @@ import redis.clients.jedis.Jedis;
 
 
 @Component
-public class JWTInterceptor implements HandlerInterceptor{
+public class JWTInterceptor implements HandlerInterceptor {
 
     public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
             throws Exception {
@@ -29,16 +31,31 @@ public class JWTInterceptor implements HandlerInterceptor{
     }
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws IOException {
-        String userId = request.getHeader("userId");
         String token = request.getHeader("token");
-        //Jedis jedis = JedisUtils.getToken(userId);
+        if (token == null || token.equals("")) {
+            returnErrorResponse(response, -1,"没有身份令牌，请重新登录");
+            return false;
+        }
+        Decrypt decrypt = new Decrypt();
+        DecodedJWT decodedJWT = decrypt.deToken(token);//token解密id
+        String userId = decodedJWT.getClaim("userId").asString();
+        String localToken = JedisUtils.getToken(userId);
         /** 您的处理逻辑 */
 
+        if (localToken == null || localToken.equals("")) {
+            returnErrorResponse(response,-1, "身份令牌已失效，请重新登录");
+            return false;
+        } else if (!token.equals(localToken)) {
+            returnErrorResponse(response, -1,"身份令牌不符");
+            return false;
+        } else {
 
-        return false;
+            return true;
+        }
     }
-    private ServerResponse returnErrorResponse(HttpServletResponse response, String result) {
-        return new ServerResponse(0,result);
+
+    private ServerResponse returnErrorResponse(HttpServletResponse response,int code, String result) {
+        return new ServerResponse(code, result);
     }
 
 
